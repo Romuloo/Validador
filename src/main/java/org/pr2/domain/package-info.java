0ce9407603ecca8ea0622b4a41f6a9fc7f6f1992
Javier Linares <j.linares5@usp.ceu.es>
2019-05-21 02:41:14 -0400

    Comentario de paquetes listo

---
+++ src/main/java/org/pr2/domain/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Este paquete contiene las clases del validador de doble Master <i>Asignatura</i>, <i>Master</i> <i>DobleMaster</i>.
+ */
+	package org.pr2.domain;
+
---
+++ src/test/java/org/pr2/dominio/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Este paquete contiene la clase <i>DobleMasterTest</i> donde se encuentran los tests.
+ */
+	package org.pr2.dominio;
